148286024444829ff049fcb6e16aa53d050b6f64
novitasalsabila <novnovv10@gmail.com>
2024-12-20 11:51:22 +0700

    Membuat database untuk menyimpan dan mengelola data Dosen

--- app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -1,4 +1,32 @@
 package com.example.ucp2.data.database
 
-class KrsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+
+
+@Database(entities = [Dosen::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase() {
+
+    // Mendefinisikan fungsi untuk mengakses data Dosen, dan MataKuliah
+    abstract fun DosenDao(): DosenDao
+
+    companion object {
+        @Volatile // Memastikan bahwa nilai variabel Instance selalu sama di semua thread
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java, // Class database
+                    "KrsDatabase" // Nama database
+                )
+                    .build().also { Instance = it }
+            }
+        }
+    }
 }
