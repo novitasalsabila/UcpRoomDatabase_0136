b55f550cdbe3815ff68293f249b04b5cdabc43cb
novitasalsabila <novnovv10@gmail.com>
2024-12-21 17:15:32 +0700

    ContainerApp bertugas menyediakan objek-objek repository yang digunakan oleh aplikasi.

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,25 @@
 package com.example.ucp2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2.data.database.KrsDatabase
+import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMatakuliah
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMatakuliah
+
+interface InterfaceContainerApp{
+    val repositoryDosen: RepositoryDosen
+    val repositoryMataKuliah: RepositoryMatakuliah
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp{
+    // Repository Dosen
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).DosenDao())
+    }
+
+    // Repository MataKuliah
+    override val repositoryMataKuliah: RepositoryMatakuliah by lazy {
+        LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).MatakuliahDao())
+    }
+}
