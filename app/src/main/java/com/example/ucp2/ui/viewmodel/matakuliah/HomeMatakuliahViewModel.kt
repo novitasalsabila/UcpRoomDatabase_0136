9dc77d25f37e7b60b2642481a0b0592c02a7edeb
novitasalsabila <novnovv10@gmail.com>
2024-12-21 19:28:34 +0700

    Halaman ini buat manajemen data detail mata kuliah biar tampilan di layar selalu up-to-date dan user bisa menghapus data kalau perlu.

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.ucp2.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMatakuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMatakuliahViewModel(
+    private  val repositoryMatakuliah: RepositoryMatakuliah
+) : ViewModel(){
+    val homeMatkulUiState: StateFlow<HomeUiState> = repositoryMatakuliah.getAllMatakuliah().filterNotNull().map{
+        HomeUiState(
+            listMatakuliah = it.toList(),
+            isLoading = false,
+        )
+    }.onStart{
+        emit(HomeUiState(isLoading = true))
+        delay(900)
+    }.catch{
+        emit(
+            HomeUiState(
+                isLoading = false,
+                isError = true,
+                errorMessage = it.message?: "Terjadi Kesalahan"
+            )
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = HomeUiState(
+            isLoading = true
+        )
+    )
+}
+
+data class HomeUiState(
+    val listMatakuliah :List<Matakuliah> = listOf(),
+    val isLoading:Boolean = false,
+    val isError :Boolean = false,
+    val errorMessage : String = ""
+)
