16775e3a2e9f93f785fcae366823f0c02840cfb6
novitasalsabila <novnovv10@gmail.com>
2024-12-21 16:23:15 +0700

    Membuat file AlamatNvigasi untuk mengatur rute halaman dan bagaimana halaman tersebut bisa diakses dengan parameter tertentu

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,42 @@
 package com.example.ucp2.ui.navigation
 
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHomeMenu : AlamatNavigasi {
+    override val route = "home_menu"
+}
+
+object DestinasiHomeMatakuliah : AlamatNavigasi {
+    override val route = "home_matakuliah"
+}
+
+object DestinasiHomeDosen : AlamatNavigasi {
+    override val route = "home_dosen"
+}
+
+//object DestinasiInsertDosen {
+//    const val route = "insert_dosen"
+//}
+
+//object DestinasiInsertMatakuliah {
+//    const val route = "insert_matakuliah"
+//}
+object  DestinasiDetailDosen : AlamatNavigasi {
+    override val route = "detail_dosen"
+    const val NIDN = "nidn"
+    val routesWithArg = "$route/{$NIDN}"
+}
+
+object DestinasiDetailMatakuliah : AlamatNavigasi {
+    override val route = "detail_matakuliah"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiUpdateMatakuliah : AlamatNavigasi {
+    override val route = "update"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
