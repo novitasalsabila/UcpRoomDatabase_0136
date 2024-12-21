d3cd23ffce7ac6c0f56bd3d19c5ac5c3d7b09d74
novitasalsabila <novnovv10@gmail.com>
2024-12-21 17:35:22 +0700

    mendefinisikan dan mengelola database menggunakan Room. Class ini menyediakan DAO untuk mengakses entitas Dosen dan Matakuliah

--- app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -5,14 +5,18 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MatakuliahDao
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.Matakuliah
 
 
-@Database(entities = [Dosen::class], version = 1, exportSchema = false)
+@Database(entities = [Dosen::class, Matakuliah::class], version = 1, exportSchema = false)
 abstract class KrsDatabase : RoomDatabase() {
 
     // Mendefinisikan fungsi untuk mengakses data Dosen, dan MataKuliah
     abstract fun DosenDao(): DosenDao
+
+    abstract fun MatakuliahDao(): MatakuliahDao
 
     companion object {
         @Volatile // Memastikan bahwa nilai variabel Instance selalu sama di semua thread
