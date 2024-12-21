325bcd9f882a86c342d82f0fd587f1e119ebc73e
novitasalsabila <novnovv10@gmail.com>
2024-12-21 16:30:39 +0700

    Membuat LocalRepositoryDosen berfungsi untuk menghubungkan aplikasi dengan database yang menyimpan data dosen

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDosen.kt
@@ -1,4 +1,21 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryDosen {
+
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDosen(
+    private val dosenDao: DosenDao
+) : RepositoryDosen {
+    override suspend fun insertDosen(dosen: Dosen) {
+        dosenDao.insertDosen(dosen)
+    }
+
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+    override fun getDosen(nidn: String): Flow<Dosen> {
+        return dosenDao.getDosen(nidn)
+    }
 }
