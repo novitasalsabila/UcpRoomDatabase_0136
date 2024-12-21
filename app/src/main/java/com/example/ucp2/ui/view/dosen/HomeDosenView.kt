b464073a5357bc22722ee7a8da2537ffca3a72e1
novitasalsabila <novnovv10@gmail.com>
2024-12-21 16:57:47 +0700

    Menampilkan konten utama berdasarkan status data yang diterima dari homeUiState (misalnya loading, error, atau data dosen).

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
@@ -88,3 +88,64 @@ fun HomeDosenView (
     }
 }
 
+@Composable
+fun BodyHomeDosenView (
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listDsn.isEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar mahasiswa
+            ListDosen(
+                listDsn = homeUiState.listDsn,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
