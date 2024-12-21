e55970b654b2da557bfa1a7d67de5ce004a483e4
novitasalsabila <novnovv10@gmail.com>
2024-12-21 18:12:21 +0700

    updatematakuliah untuk menampilkan tampilan halaman update mata kuliah.

---
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/UpdateMatakuliahView.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.TopAppBar
+import com.example.ucp2.ui.viewmodel.dosen.PenyediaViewModelDosen
+import com.example.ucp2.ui.viewmodel.matakuliah.UpdateMataKuliahViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateMataKuliahView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMataKuliahViewModel = viewModel(factory = PenyediaViewModelDosen.Factory)
+) {
