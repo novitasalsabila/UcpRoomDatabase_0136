0002e85dbd5c928d8e35dcfb26713d170323cd0d
novitasalsabila <novnovv10@gmail.com>
2024-12-21 16:27:13 +0700

    Membuat file RepositoryDosen untuk mengelola data dosen, seperti menambah data dosen, mengambil daftar semua dosen, dan mencari dosen berdasarkan NIDN

--- app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -1,4 +1,12 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDosen {
+    suspend fun insertDosen(dosen: Dosen)
+    fun getAllDosen(): Flow<List<Dosen>>
+    fun getDosen(nidn: String): Flow<Dosen>
+//    suspend fun deleteDosen(dosen: Dosen)
+//    suspend fun updateDosen(dosen: Dosen)
 }
