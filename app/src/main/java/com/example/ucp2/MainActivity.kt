fb76b280e5c98e12e256b836f9fadf7c0fafca3f
novitasalsabila <novnovv10@gmail.com>
2024-12-21 20:27:29 +0700

    Memanggil PengelolahHalaman pada file main activity

--- app/src/main/java/com/example/ucp2/MainActivity.kt
+++ app/src/main/java/com/example/ucp2/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp2.ui.navigation.PengelolaHalaman
 import com.example.ucp2.ui.theme.UCP2Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UCP2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PengelolaHalaman(modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
